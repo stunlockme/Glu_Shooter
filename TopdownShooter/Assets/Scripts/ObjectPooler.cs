91d2dcef48c5d5641f76fb5f6f3ce40d7b5532a0
Teja <thunderwrath@gmail.com>
2020-04-26 14:43:14 +0530

    Add - Objectpool to reuse objects.

---
+++ TopdownShooter/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : Singleton<ObjectPooler>
+{
+    [System.Serializable]   //expose class in inspector.
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    private GameObject parent;
+    public GameObject PoolParent { get { return parent; } private set {; } }
+
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+        parent = GameObject.Find("PooledObjects");
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.transform.SetParent(parent.transform);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    /// <summary>
+    /// spawn gameobject from pool.
+    /// </summary>
+    /// <param name="tag">name of obj in pool</param>
+    /// <param name="position">pos of obj to spawn at</param>
+    /// <param name="rotation">rotation of obj to spawn with</param>
+    /// <param name="parent">parent of obj</param>
+    /// <returns>gameobject</returns>
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation, Transform parent)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+            return null;
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+        objectToSpawn.transform.SetParent(parent);
+        objectToSpawn.SetActive(true);
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+        return objectToSpawn;
+    }
+}
